8abb36d5699b233b23dff1bd2c39a93866a13415
Zijian Li <zijian.li@sjsu.edu>
2021-05-10 14:48:37 -0700

    form.css added

--- src/components/index.component.js
+++ src/components/index.component.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './form.css';
+
 
 
 export default class Index extends Component {
@@ -70,40 +73,75 @@ export default class Index extends Component {
       
     render() {
         return (
-            <div className="contact">
-                <form className="form" onSubmit={this._handleSubmit} id="formContact">
-                
-                <label>
-                    Student Name:
-                    <input type="text" id="student" name ="student" value={this.state.studentName} onChange={this._handleChangeName} required/><br/>
-                    Student ID:
-                    <input type="text" id="studentid" name="studentid" value={this.state.studentID} onChange={this._handleChangeID} required/><br/>
-                    Student Status:
-                    <select value={this.state.studentStatus} onChange={this._handleChangeStatus} required>
-                        <option default value="">-</option>
-                        <option value="freshman">Freshman</option>
-                        <option value="sophomore">Sophomore</option>
-                        <option value="junior">Junior</option>
-                        <option value="senior">Senior</option>
-                    </select><br/>
-                    Student Major:
-                    <select name="major" value={this.state.studentMajor} onChange={this._handleChangeMajor} required>
-                        <option default value="">-</option>
-                        <option value ="cs">Computer Sciences</option>
-                        <option value ="ee">Electrical Engineering</option>
-                        <option value ="math">Math</option>
-                        <option value ="econ">Economics</option>
-                    </select><br/>
-                    Course History: 
-                    <textarea rows="5" cols="30" placeholder="Example: 
-                    CS 157C
-                    CS 100W" 
-                    name="History" value={this.state.studentCH} onChange={this._handleChangeCH}></textarea>
-                </label>
-                <br/>
-                <input type="submit" value="Find course Plan" />
-            </form>
+            
+            <div class="form-body">
+                <div class="row">
+                    <div class="form-holder">
+                        <div class="form-content">
+                            <div class="form-items">
+                                <h3>Student information</h3>
+                                <p>Fill in the data below.</p>
+                                <form className="form" onSubmit={this._handleSubmit} id="formContact">
+                                    <div class="col-md-12">
+                                    <input class="form-control" type="text" name="student" placeholder="Student Name" onChange={this._handleChangeName} required/>
+                                    <div class="valid-feedback">Student name field is valid!</div>
+                                    <div class="invalid-feedback">Student name field cannot be blank!</div>
+                                </div>
+    
+                                <div class="col-md-12">
+                                    <input class="form-control" type="text" name="studentid" placeholder="Student ID" onChange={this._handleChangeID} required/>
+                                     <div class="valid-feedback">Student ID field is valid!</div>
+                                     <div class="invalid-feedback">Student ID  field cannot be blank!</div>
+                                </div>
+    
+                               <div class="col-md-12">
+                                    <select class="form-select mt-3" onChange={this._handleChangeStatus} required>
+                                          <option selected disabled value="">Student Status</option>
+                                          <option value="freshman">Freshman</option>
+                                          <option value="sophomore">Sophomore</option>
+                                          <option value="junior">Junior</option>
+                                          <option value="senior">Senior</option>
+                                   </select>
+                                    <div class="valid-feedback">You selected a status!</div>
+                                    <div class="invalid-feedback">Please select a status!</div>
+                               </div>
+                                
+                                
+                                <div class="col-md-12">
+                                    <select class="form-select mt-3" onChange={this._handleChangeMajor} required>
+                                          <option selected disabled value="">Student Major</option>
+                                          <option value ="cs">Computer Sciences</option>
+                                          <option value ="ee">Electrical Engineering</option>
+                                          <option value ="math">Math</option>
+                                          <option value ="econ">Economics</option>
+                                   </select>
+                                    <div class="valid-feedback">You selected a major!</div>
+                                    <div class="invalid-feedback">Please select a major!</div>
+                               </div>
+                                
+                                <div class="col-md-12">
+                                    <textarea rows="5" cols="30" placeholder="Student History - one course per line" 
+                        name="History" value={this.state.studentCH} onChange={this._handleChangeCH}></textarea>
+                                    
+                               </div>
+    
+    
+                               
+    
+                            
+                      
+    
+                                <div class="form-button mt-3">
+                                    <button id="submit" type="submit" class="btn btn-primary">Find course Plan</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+
+    
             
             
         );
